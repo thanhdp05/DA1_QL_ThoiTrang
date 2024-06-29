47b5687b8e7e039aa88f6066613eddaca98797b4
thanhdp05 <Thanhphuocdinh05@gmail.com>
2024-06-30 01:23:06 +0700

    create src code

---
+++ DuAn1/src/main/java/com/mycompany/duan1/DuAn1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.duan1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class DuAn1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
